bbdef77577725d938f28490ad95cd560466ab79d
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2017-08-31 12:39:52 +0100

    Update dashdoorbell.py

--- dashdoorbell.py
+++ dashdoorbell.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# This was based on http://bit.ly/keschy-dash by Keschy but updated for                   #
+# indentation formatting and to remove unnecessary console outputs for running headless.  #
+# I have also added additional API parameters, such as URL, HTML and sound.               #
+
 import datetime
 import logging
 import urllib
@@ -9,14 +13,12 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
   
 def button_pressed_dash1():
-    #current_time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d_%H:%M:%S')
-    #print 'Button Pressed at ' + current_time
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
         urllib.urlencode({
-            "token":"PUSHOVER_API_TOKEN",
-            "user":"PUSHOVER_USER_TOKEN",
-            "html":"0",	# 1 to enable
+            "token":"PUSHOVER_API_TOKEN",       # Get this from your Pushover app
+            "user":"PUSHOVER_USER_TOKEN",       # Get this from your Pushover account
+            "html":"0",	                        # 1 to enable
             "title":"Doorbell!",
             "message":"There's somebody at the door!",
             "url":"https://ADDYOUROWN.URL",
@@ -36,8 +38,6 @@ def udp_filter(pkt):
   
 mac_to_action = {'xx:xx:xx:xx:xx:xx' : button_pressed_dash1} # Add MAC address in lowercase
 mac_id_list = list(mac_to_action.keys())
-  
-#print "Waiting for a button press..."
 sniff(prn=udp_filter, store=0, filter="udp", lfilter=lambda d: d.src in mac_id_list)
   
 if __name__ == "__main__":
